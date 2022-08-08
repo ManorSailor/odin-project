705bd1be8f86b7b02a14e658c80d2e08c4a2ed22
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-08-08 20:10:30 +0530

    Signup-Form: Add comments to JS code

--- odin-javascript/odin-signup-form/js/app.js
+++ odin-javascript/odin-signup-form/js/app.js
@@ -1,14 +1,25 @@
+// Global Elements
 const confPassLabel = document.querySelector('label[for="conf-pass"]');
 const confPassInfo = document.getElementById('pass-info');
 const passwordFields = document.getElementsByClassName('pass');
+
+// Global Variables
 let obj = {};
 
+// For each password field, listen for input in them
 [...passwordFields].forEach(field => field.addEventListener('input', validatePassword));
 
+// Function which validates the password
+// Using an object allows us to keep both passwords in the same place & we don't need separate variables for them
 function validatePassword(e) {
-    obj[e.target.id] = e.target.value;
+    // Store the password in an object with key being the id of input field 
+    // /* Eg: obj[pass] = 'asdd' */
+    obj[e.target.id] = e.target.value; 
     
+    // Ensure both passwords exist before checking if they are the same
     if (obj["pass"] && obj["conf-pass"]) {
+        // Make sure passwords exactly match with each other, we check both twice because includes method
+        // doesn't exactly check for a match i.e, 'aaaaa'.includes('aaa'); returns true. We don't want this.
         const isSame = obj["pass"].includes(obj["conf-pass"]) && obj["conf-pass"].includes(obj["pass"]);
 
         if (isSame) {
