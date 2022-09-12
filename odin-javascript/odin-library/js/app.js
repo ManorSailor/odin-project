7da8b4928a45b025d7dfdd32d30764d13d6804e9
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-09-12 13:53:34 +0530

    Library: Implement functionality to retrieve data from the form

--- odin-javascript/odin-library/js/app.js
+++ odin-javascript/odin-library/js/app.js
@@ -6,7 +6,27 @@ modal.addEventListener('click', toggleModal);
 newBookBtn.addEventListener('click', toggleModal);
 
 function toggleModal(e) {
-    if (e.target === modal || e.target === newBookBtn) {
+    if (e.target === modal || e.target === newBookBtn || e.type === 'submit') {
         modal.classList.toggle('active');
     }
 }
+
+// Retrieve data from input fields when the form is submitted
+const form = document.getElementById('form')
+
+function parseData(formFields) {
+    const len = formFields.length - 1;
+    const data = [];
+
+    for (let i = 0; i < len; i++) {
+        data.push(formFields[i].value);
+    }
+
+    return data;
+}
+
+form.addEventListener('submit', (e) => {
+    const [name, author, pages, status] = parseData(e.target);
+    toggleModal(e);
+    e.preventDefault();
+});
