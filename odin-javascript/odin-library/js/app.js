2956857e6e0277f9fbf9bb464b964f5073ddc843
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-09-12 20:37:41 +0530

    Library: Verbose card factory function
    Codebase will be refactored later, first we need to achieve MVP.
    This is really bad code here, card function will be refactored in next commit

--- odin-javascript/odin-library/js/app.js
+++ odin-javascript/odin-library/js/app.js
@@ -1,6 +1,7 @@
 // Global Variables
 const modal = document.getElementById('modal');
 const newBookBtn = document.getElementById('new-book');
+const cardsContainer = document.querySelector('.cards-container');
 let library = [];
 
 modal.addEventListener('click', toggleModal);
@@ -34,6 +35,8 @@ form.addEventListener('submit', (e) => {
     const parsedData = parseData(e.target);
     const newBook = new Book(parsedData);
     library.push(newBook);
+    const newCard = cardFactory(newBook);
+    cardsContainer.appendChild(newCard);
     toggleModal(e);
     e.preventDefault();
 });
@@ -55,4 +58,62 @@ Book.prototype.changeStatus = function() {
 
 Book.prototype.removeBook = function() {
     library = library.filter(book => (book.id !== this.id));
+}
+
+// Card Factory Function
+function cardFactory({id, title, author, pages, hasRead}) {
+    const article = document.createElement('article');
+
+    // Card Header
+    const header = document.createElement('header');
+
+    const h3 = document.createElement('h3');
+    h3.classList.add('title');
+    h3.textContent = title;
+
+    const img = document.createElement('img');
+    img.classList.add('remove-book');
+    img.src = "./assets/delete.png";
+    img.alt = "Delete icon";
+
+    header.classList.add('card-header');
+    header.append(h3, img);
+
+    // Card body
+    const h4 = document.createElement('h4');
+    h4.classList.add('author');
+    h4.textContent = author;
+
+    const p = document.createElement('p');
+    p.classList.add('pages');
+    p.textContent = `${pages} pages`;
+
+    // Card toggle
+    const label = document.createElement('label');
+
+    const input = document.createElement('input');
+    input.classList.add('mark-as-read');
+    input.type = 'checkbox';
+    input.checked = hasRead;
+
+    const toggleContainer = document.createElement('span');
+
+    const knob = document.createElement('span');
+    knob.classList.add('knob');
+
+    const bar = document.createElement('span');
+    bar.classList.add('bar');
+
+    toggleContainer.classList.add('toggle-container');
+    toggleContainer.append(knob, bar);
+
+    label.textContent = 'Mark as read';
+    label.classList.add('read-status');
+    label.append(input, toggleContainer);
+
+    article.setAttribute('data-id', id);
+    article.classList.add('card');
+    article.append(header, h4, p, label);
+
+    return article;
 }
