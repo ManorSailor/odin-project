c5ed49245d938362840978f1ffd304137618a586
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-09-26 19:46:58 +0530

    TTT: Start game modal

--- odin-javascript/odin-tic-tac-toe/js/main.js
+++ odin-javascript/odin-tic-tac-toe/js/main.js
@@ -3,3 +3,18 @@ import * as utils from './utils/utils.js'
 
 /* ========= Global Variables ========= */
 const body = document.body;
+
+// Modal
+function makeModal(txt='', btnTxt='') {
+
+}
+
+const modal = utils.createElement('section', '', ['intro-modal']);
+
+const text = utils.createElement('p', "Let's Play The Classic Game of Tic-tac-toe!", ['intro-text']);
+
+const btn = utils.createElement('button', 'Start Game!', ['btn']);
+
+modal.appendChild(text);
+modal.appendChild(btn);
+body.appendChild(modal);
--- odin-javascript/odin-tic-tac-toe/js/utils/utils.js
+++ odin-javascript/odin-tic-tac-toe/js/utils/utils.js
@@ -1,6 +1,9 @@
-export function createElement(type, classes=[]) {
+export function createElement(type, text = '', classes = []) {
     // Create the passed element
     const element = document.createElement(type);
+
+    // Attach text to the element
+    element.textContent = text;
 
     // Attach classes to element
     classes.forEach(item => element.classList.add(item));
