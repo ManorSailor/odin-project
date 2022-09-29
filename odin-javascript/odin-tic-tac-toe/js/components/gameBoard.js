76ddc6ccbdb61c8167783cf2f70863de3eb43737
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-09-29 23:12:35 +0530

    TTT: Initial gameBoard Object implementation

---
+++ odin-javascript/odin-tic-tac-toe/js/components/gameBoard.js
@@ -0,0 +1,72 @@
+const main = document.querySelector('.game-container');
+
+const view = (() => {
+    const section = document.createElement('section');
+    section.classList.add('game-board');
+
+    for (let i = 0; i < 9; i++) {
+        const cell = document.createElement('div');
+        cell.setAttribute('data-cell-id', i);
+        section.appendChild(cell);
+    }
+
+    const insert = (gameObject, cell) => {
+        if (cell === section) return;
+        cell.textContent = gameObject;
+        cell.classList.add('full');
+    };
+
+    const clear = () => {
+        let child = section.firstChild;
+        while (child) {
+            child.textContent = '';
+            child.classList = "";
+            child = child.nextSibling;
+        }
+    }
+
+    main.appendChild(section);
+
+    return { insert, clear };
+})();
+
+const model = (() => {
+    // Set is more performant & helped us out a bit in implementation
+    // https://www.reddit.com/r/learnjavascript/comments/n1j7ub/comment/gwdpz0h/?utm_source=share&utm_medium=web2x&context=3
+    const board = new Set();
+
+    const isBoardFull = () => (board.size === 9);
+
+    const isCellTaken = (cell) => board.has(cell);
+
+    const insert = (cell) => {
+        if (!isCellTaken(cell)) {
+            board.add(cell);
+            return true;
+        }
+        return false;
+    };
+    
+    const clear = () => board.clear();
+
+    return { isBoardFull, insert, clear };
+})();
+
+export const gameBoard = (() => {
+    const insert = (gameObject, cell) => {
+        const inserted = model.insert(cell);
+        if (inserted) {
+            view.insert(gameObject, cell);
+        }
+        return inserted;
+    }
+
+    const isFull = () => model.isBoardFull();
+
+    const clear = () => {
+        view.clear();
+        model.clear();
+    }
+
+    return { insert, isFull, clear, };
+})();
--- odin-javascript/odin-tic-tac-toe/js/main.js
+++ odin-javascript/odin-tic-tac-toe/js/main.js
@@ -1,40 +1,14 @@
 /* ========= Imports ========= */
-import * as utils from './utils/utils.js'
+import { gameBoard } from "./components/gameBoard.js";
 
 /* ========= Global Variables ========= */
-const body = document.body;
-const main = document.createElement('main');
-main.classList.add('game-container');
-body.appendChild(main);
+const boardContainer = document.querySelector('.game-board');
 
 /* ========= Game Board Object ========= */
-const gameBoard = (() => {
-    const render = (() => {
-        const section = document.createElement('section');
-        section.classList.add('game-board');
-
-        for (let i = 0; i < 9; i++) {
-            const child = document.createElement('div');
-            child.classList.add('cell');
-            child.setAttribute('data-id', i);
-            section.appendChild(child);
-        }
-
-        main.appendChild(section);
-    })();
-    
-    let occupiedCells = [];
-
-    const isBoardFull = () => (occupiedCells.length === 9);
-
-    const isCellTaken = (cell) => Boolean(occupiedCells[cell]);
-
-    const insert = (gameObject, cell) => {
-        cell.appendChild(gameObject.name);
-        occupiedCells.push(cell);
-    };
-
-    const clearBoard = () => occupiedCells = [];
-
-    return { isBoardFull, isCellTaken, clearBoard };
-})();
+boardContainer.addEventListener('click', (e) => {
+    if (!gameBoard.isFull()) {
+        gameBoard.insert('X', e.target);
+    } else {
+        gameBoard.clear();
+    }
+});
