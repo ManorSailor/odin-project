3ac5d084413ddd140ee181acb35c5839f4dd9649
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-10-05 17:15:13 +0530

    TTT: Utils Cleanup

--- odin-javascript/odin-tic-tac-toe/js/utils/utils.js
+++ odin-javascript/odin-tic-tac-toe/js/utils/utils.js
@@ -6,20 +6,6 @@ export const GRID_SIZE = 9;
 export const winningPaths = [];
 
 /* ========= Utilities ========= */
-function createElement([type, text = '', classes = []]) {
-    // Create the passed element
-    const element = document.createElement(type);
-
-    // Attach text to the element
-    element.textContent = text;
-
-    // Attach classes to element
-    classes.forEach(item => element.classList.add(item));
-
-    // Return the element
-    return element;
-}
-
 // Sleep function to wait for x seconds. Note: Must be used with await/async!
 // Thanks to: https://stackoverflow.com/a/39914235
 export const sleep = ms => new Promise(r => setTimeout(r, ms));
@@ -114,33 +100,3 @@ const generatePaths = (() => {
         }
     }
 })();
-
-function parseObject(obj) {
-    let arr = [];
-    let parentNode = arguments[1];
-    let element;
-
-    for (let prop in obj) {
-        if (obj[prop] !== 'childNodes') {
-            arr.push(obj[prop]);
-        }
-
-        if (prop === 'childNodes' && obj[prop]) {
-            element = createElement(arr);
-            if (parentNode) {
-                parentNode.append(element);
-            }
-            parentNode = element;
-
-            obj[prop].forEach(o => parseObject(o, parentNode));
-
-            return parentNode;
-        } else if (prop === 'childNodes' && !obj[prop]) {
-            parentNode.append(createElement(arr));
-        }
-    }
-}
-
- export function make(obj) {
-    return parseObject(obj);
- }
