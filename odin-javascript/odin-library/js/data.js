f088cd578088407d4d545e67fd0d40e49df6573b
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-10-24 17:43:08 +0530

    Library: Update demo books

--- odin-javascript/odin-library/js/data.js
+++ odin-javascript/odin-library/js/data.js
@@ -1,33 +1,33 @@
 // File containing some example books
 export const booksList = [
-    [
-        "Things Fall Apart",
-        "Chinua Achebe",
-        209,
-        true
-    ],
-    [
-        "Fairy tales",
-        "Hans Christian Andersen",
-        784,
-        false
-    ],
-    [
-        "The Divine Comedy",
-        "Dante Alighieri",
-        928,
-        true
-    ],
-    [
-        "The Epic Of Gilgamesh",
-        "Unknown",
-        160,
-        false
-    ],
-    [
-        "The Book Of Job",
-        "Unknown",
-        176,
-        true
-    ],
+    {
+        "author": "Chinua Achebe",
+        "pages": 209,
+        "title": "Things Fall Apart",
+        "hasRead": true,
+      },
+      {
+        "author": "Hans Christian Andersen",
+        "pages": 784,
+        "title": "Fairy tales",
+        "hasRead": false,
+      },
+      {
+        "author": "Dante Alighieri",
+        "pages": 928,
+        "title": "The Divine Comedy",
+        "hasRead": true,
+      },
+      {
+        "author": "Unknown",
+        "pages": 160,
+        "title": "The Epic Of Gilgamesh",
+        "hasRead": false,
+      },
+      {
+        "author": "Unknown",
+        "pages": 176,
+        "title": "The Book Of Job",
+        "hasRead": false,
+      },
 ]
