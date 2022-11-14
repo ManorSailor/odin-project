dcf794a0dda873495ec1e52d969e2a7f179a0bd0
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-11-14 16:21:48 +0530

    Restaurant: Setup tailwind css

--- odin-javascript/odin-restaurant-page/webpack.config.js
+++ odin-javascript/odin-restaurant-page/webpack.config.js
@@ -10,4 +10,12 @@ module.exports = {
     devServer: {
         static: './dist',
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', 'postcss-loader']
+            },
+        ],
+    },
 };
