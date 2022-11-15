57725a4e2345cce339844ad12981dcfff272fab5
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-11-15 13:33:00 +0530

    Restaurant: Refactor makeElement utility
    Now. We only have to pass in a JS object literal containing an element's
    properties. Future changes may further enhance makeElement utility

--- odin-javascript/odin-restaurant-page/src/js/utilities/utils.js
+++ odin-javascript/odin-restaurant-page/src/js/utilities/utils.js
@@ -3,21 +3,26 @@ const body = document.body;
 
 /**
  * Creates & return DOM elements
- * @param {string} type              - Type of element, i.e, div, main etc
- * @param {string} [textContent]     - Element's textContent
- * @param {Object} [attr]            - Object containing element's attributes
- * @param {string} [attr.id]         - Element's ID
- * @param {string} [attr.class]      - Element's classList
- * @param {*} [attr.dataAttr]        - Element's data attributes
+ * @param {Object} elementProps                     - JS Object, contains info required to make a DOM Node
+ * @param {string} elementProps.type                - Element's Type
+ * @param {string} [elementProps.textContent]       - Element's textContent
+ * @param {Object} [elementProps.attr]              - JS Object, contains element's attributes
+ * @param {string} [elementProps.attr.id]           - Element's ID
+ * @param {string|Array} [elementProps.attr.class]  - Element's classList
+ * @param {*} [elementProps.attr.dataAttr]          - Element's data attributes
  * @returns {Node}
  */
-function makeElement(type, textContent='', attr={}) {
-    const element = document.createElement(type);
+function makeElement(elementProps={}) {
+    if (!elementProps.type) throw 'Element\s type is undefined. Make sure you provide an HTML element type'
 
-    if (textContent) element.textContent = textContent;
+    const element = document.createElement(elementProps.type);
 
-    for (const key in attr) {
-        element.setAttribute(key, attr[key]);
+    if (elementProps.textContent) element.textContent = textContent;
+
+    if (elementProps.attr) {
+        for (const key in elementProps.attr) {
+            element.setAttribute(key, elementProps.attr[key]); // TODO: Write an array/string handler for classList
+        }
     }
 
     return element;
