741519c70b211065cee4f2479ef4e81741d34267
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-11-16 14:23:18 +0530

    Restaurant: Implement tabsHandler & render method

--- odin-javascript/odin-restaurant-page/src/main.js
+++ odin-javascript/odin-restaurant-page/src/main.js
@@ -2,5 +2,29 @@ import './css/styles.css'
 import navbar from './js/components/common/navbar';
 import mainContainer from './js/components/common/mainContainer';
 
-navbar.attach();
-mainContainer.attach();
+navbar.init(tabHandler);
+renderTab()
+
+// Handles which tab to render
+function tabHandler(activeTab) {
+    if (activeTab === '#menu') {
+        renderTab()
+    } else if (activeTab === '#contact') {
+        renderTab();
+    } else if (activeTab === '#home') {
+        renderTab();
+    }
+}
+
+// Handles rendering of the passed tab
+function renderTab(tab) {
+    // Clear the mainContainer
+    mainContainer.remove();
+    
+    // Generate the passed tab & get its nodes
+
+    // Append each node to the main container
+
+    // Reattach mainContainer to body
+    mainContainer.attach()
+}
