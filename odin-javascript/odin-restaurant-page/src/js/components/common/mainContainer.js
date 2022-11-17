13aecb4d1f6beef51a2928f37f37fc3f72029a41
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-11-17 17:59:01 +0530

    Restaurant: Refactor mainContainer.js to remove useless logic

--- odin-javascript/odin-restaurant-page/src/js/components/common/mainContainer.js
+++ odin-javascript/odin-restaurant-page/src/js/components/common/mainContainer.js
@@ -3,10 +3,11 @@ import { mainContainer as main } from "./mainContainer/mainContainerNode";
 
 const mainContainer = (() => {
     const clear  = () => [...main.children].forEach(child => child.remove());
-    const attach = () => body.appendChild(main);
     const append = (...nodes) => nodes.forEach(node => main.appendChild(node));
 
-    return { clear, attach, append };
+    return { clear, append };
 })();
+
+body.appendChild(main);
 
 export default mainContainer;
