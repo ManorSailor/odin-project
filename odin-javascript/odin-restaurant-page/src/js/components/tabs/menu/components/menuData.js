bc2411bd876b7c4f39f71168fd4362314b2d8d89
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-11-17 17:49:15 +0530

    Restaurant: Finalize menu tab styles

---
+++ odin-javascript/odin-restaurant-page/src/js/components/tabs/menu/components/menuData.js
@@ -0,0 +1,54 @@
+const MainDishes = [
+    {
+        'name': 'Special Crab',
+        'price': '$30',
+        'desc': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi vero assumenda dolor eius maxime natus!',
+    },
+    {
+        'name': 'Spicy Chicken',
+        'price': '$20',
+        'desc': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi vero assumenda dolor eius maxime natus!',
+    },
+    {
+        'name': 'Grilled Squirrels',
+        'price': '$25',
+        'desc': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi vero assumenda dolor eius maxime natus!',
+    },
+    {
+        'name': 'Garden Salad',
+        'price': '$8',
+        'desc': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi vero assumenda dolor eius maxime natus!',
+    },
+];
+
+const Desserts = [
+    {
+        'name': 'Cream Puff',
+        'price': '$6',
+        'desc': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi vero assumenda dolor eius maxime natus!',
+    },
+    {
+        'name': 'Hot Chocolate Cake',
+        'price': '$5',
+        'desc': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi vero assumenda dolor eius maxime natus!',
+    },
+];
+
+const Drinks = [
+    {
+        'name': 'Kiwi Cocktail',
+        'price': '$15',
+        'desc': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi vero assumenda dolor eius maxime natus!',
+    },
+    {
+        'name': 'Icy Beer',
+        'price': '$20',
+        'desc': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi vero assumenda dolor eius maxime natus!',
+    },
+];
+
+export {
+    MainDishes,
+    Desserts,
+    Drinks,
+}
--- odin-javascript/odin-restaurant-page/src/js/components/tabs/menu/components/menuNodes.js
+++ odin-javascript/odin-restaurant-page/src/js/components/tabs/menu/components/menuNodes.js
@@ -1,5 +1,5 @@
 import { makeElement } from "../../../../utilities/utils";
-import { headerImgObj, contentWrapperObj, menuWrapperObj, overlayObj, textContainerObj, introTextObj, introHeaderObj, menuContainerObj, menuHeaderObj, menuOlObj, menuLiObjs, menuBtnObjs } from "./menuObjects";
+import { headerImgObj, contentWrapperObj, menuWrapperObj, overlayObj, textContainerObj, introTextObj, introHeaderObj, menuContainerObj, menuHeaderObj, menuOlObj, menuLiObjs, menuBtnObjs, menuItemsContainerObj, MainDishesContainerObj, DessertsContainerObj, menuListContainerObj, DrinksContainerObj,  } from "./menuObjects";
 
 const menuWrapper = makeElement(menuWrapperObj);
 
@@ -31,11 +31,85 @@ menuLiObjs.forEach((liObj, index) => {
 menuOl.append(...liNodes);
 menuHeader.append(menuOl);
 
-menuContainer.append(menuHeader);
+// Menu Items List
+const menuItemsContainer = makeElement(menuItemsContainerObj);
 
+// Menu List Container
+const menuListContainer = makeElement(menuListContainerObj);
+
+const MainDishes = parseObject(MainDishesContainerObj);
+const Desserts = parseObject(DessertsContainerObj);
+const Drinks = parseObject(DrinksContainerObj);
+
+// Handles parsing the objects which makes up a menu item
+// obj - Object containing arrays of objects
+function parseObject(obj) {
+    const liNodes = [];
+    const textWrapperNodes = [];
+    const headerNodes = [];
+
+    for (const key in obj) {
+        obj[key].forEach((item, index) => {
+
+            switch (key) {
+                case 'menuItemsLiObjs':
+                    const liNode = makeElement(item);
+                    liNodes.push(liNode);
+                    break;
+                
+                case 'menuItemsImgObjs':
+                    const imgNode = makeElement(item);
+                    liNodes[index].append(imgNode);
+                    break;
+
+                case 'menuItemsTextWrapperObjs':
+                    const textWrapperNode = makeElement(item);
+                    liNodes[index].append(textWrapperNode);
+                    textWrapperNodes.push(textWrapperNode);
+                    break;
+                
+                case 'menuItemsHeaderObjs':
+                    const headerNode = makeElement(item);
+                    textWrapperNodes[index].append(headerNode);
+                    headerNodes.push(headerNode);
+                    break;
+        
+                case 'menuItemsNameObjs':
+                    const nameNode = makeElement(item);
+                    headerNodes[index].append(nameNode);
+                    break;
+
+                case 'menuItemsPriceObjs':
+                    const priceNode = makeElement(item);
+                    headerNodes[index].append(priceNode);
+                    break;
+
+                case 'menuItemsDescObjs':
+                    const descNode = makeElement(item);
+                    textWrapperNodes[index].append(descNode);
+                    break;
+                
+                default:
+                    break;
+            }
+        });
+    }
+
+    return liNodes;
+}
+
+// By default, we will show main dishes on the menu page
+menuListContainer.append(...MainDishes);
+
+menuItemsContainer.append(menuListContainer);
+menuContainer.append(menuHeader, menuItemsContainer);
 menuWrapper.append(contentWrapper, menuContainer);
 
 export {
     menuWrapper,
     headerImg,
+    menuListContainer,
+    MainDishes,
+    Desserts,
+    Drinks,
 }
