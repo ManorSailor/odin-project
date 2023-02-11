29227ffb51b6a408193d72a0fb7055c93847e2de
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2023-02-11 17:43:08 +0530

    BST: Add delete method

--- odin-javascript/odin-cs/bst/BBST.mjs
+++ odin-javascript/odin-cs/bst/BBST.mjs
@@ -9,6 +9,16 @@ class BBST {
     this.#root = this.buildTree([...new Set(arr.sort((a, b) => a - b))]);
   }
 
+  static isLeafNode(node = null) {
+    // Make sure node has no children
+    return !node?.left && !node?.right;
+  }
+
+  static isInternalNode(node = null) {
+    // make sure node has either left OR right child
+    return (node?.left && !node?.right) || (node?.right && !node?.left);
+  }
+
   get root() {
     return this.#root;
   }
@@ -17,6 +27,13 @@ class BBST {
     if (!this.#root) {
       this.#root = node;
     }
+    return this;
+  }
+
+  clear() {
+    this.#root = null;
+    this.#height = -1;
+    this.#depth = -1;
     return this;
   }
 
@@ -74,7 +91,45 @@ class BBST {
     return find(this.#root);
   }
 
-  delete() {}
+  successor(node) {
+    if (!node) return null;
+    // Find the smallest node in the left branch(es) of right branch
+    node = node.right;
+
+    while (node.left) {
+      node = node.left;
+    }
+    return node;
+  }
+
+  delete(value) {
+    if (value == null) return null;
+
+    const { parent, branch, node } = this.find(value, true) ?? {};
+
+    if (!node) return null;
+
+    // Create a new instance of the node to be removed, in the 3rd removal case, we modify value of this node instead of removing it
+    const removedNode = TreeNode.of(node.value);
+
+    if (BBST.isLeafNode(node)) {
+      node === this.#root
+        ? this.clear()
+        : (parent[branch] = null);
+    } else if (BBST.isInternalNode(node)) {
+      node === this.#root
+        ? (this.#root = node.left || node.right)
+        : (parent[branch] = node.left || node.right);
+    } else {
+      const successor = this.successor(node);
+      // Remove the successor, if copied before removal, we get an infinite loop
+      const removedNode = this.delete(successor.value);
+      node.value = removedNode.value;
+    }
+
+    return removedNode;
+  }
+
   levelOrder() {}
   preOrder() {}
   inOrder() {}
@@ -97,11 +152,13 @@ class BBST {
 }
 
 const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-const tree = new BBST(arr);
+const arr2 = [
+  30, 20, 32, 40, 50, 60, 70, 65, 75, 80, 34, 36, 85, 28, 37, 19, 12, 13, 14,
+];
+const tree = new BBST(arr2);
 
-tree.insert(58);
-console.log(tree.find(67, true));
-
+tree.print();
+tree.delete(36)
 tree.print();
 
 export default BBST;
