44cb032fd9b2cb5603003e0e2229ec49fc7a61a8
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2023-02-16 13:39:04 +0530

    KT: Implement generateMoves method of the Knight module
    The method currently generates all the legal moves given a starting position
    and returns them as a 2D array

--- odin-javascript/odin-knight-travails/KnightTravails.js
+++ odin-javascript/odin-knight-travails/KnightTravails.js
@@ -1,1 +1,31 @@
 const isMoveValid = ([x, y]) => x >= 0 && x <= 7 && y >= 0 && y <= 7;
+
+const Knight = (() => {
+  const moveOffsets = [
+    [1, 2],
+    [-1, -2],
+    [-2, -1],
+    [2, 1],
+    [-1, 2],
+    [1, -2],
+    [-2, 1],
+    [2, -1],
+  ];
+
+  const generateMoves = ([x, y], end) => {
+    const moves = [];
+
+    for (const [a, b] of moveOffsets) {
+      const newMove = [x + a, y + b];
+      if (isMoveValid(newMove)) {
+        moves.push(newMove);
+      }
+    }
+
+    return moves;
+  };
+
+  return { generateMoves };
+})();
+
+console.log(Knight.generateMoves([3, 3], [6, 6]));
