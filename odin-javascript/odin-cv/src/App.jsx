cb3c300a5981f41dc5aa53cb184b1b849b03f3d1
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2023-04-09 17:38:59 +0530

    odin-cv: Implement AppHeader component

--- odin-javascript/odin-cv/src/App.jsx
+++ odin-javascript/odin-cv/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import './styles/styles.css';
+import AppHeader from './components/AppHeader';
+
 class App extends React.Component {
   render() {
-    return <main className=""></main>;
+    return (
+      <main className="">
+        <AppHeader />
+      </main>
+    );
   }
 }
 
---
+++ odin-javascript/odin-cv/src/components/AppHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class AppHeader extends React.Component {
+  render() {
+    return <h1 className="app-header">CV App</h1>;
+  }
+}
+
+export default AppHeader;
