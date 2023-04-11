eca7d83d3d03ea6ea3c95dc8ea15005abbf02569
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2023-04-11 23:00:02 +0530

    odin-cv: Implement QualificationsTab component

---
+++ odin-javascript/odin-cv/src/components/Tabs/QualificationsTab.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+
+class QualificationTab extends React.Component {
+  render() {
+    return (
+      <form className="tab-form">
+        <h3 className="form-title">Qualifications</h3>
+
+        <ul className="form-fields">
+          <li className="">
+            <label htmlFor="degree" className="">
+              <input type="text" name="degree" id="degree" placeholder=" " />
+              <span>Degree/Certification</span>
+            </label>
+          </li>
+
+          <li className="">
+            <label htmlFor="university" className="">
+              <input
+                type="text"
+                name="university"
+                id="university"
+                placeholder=" "
+              />
+              <span>University</span>
+            </label>
+          </li>
+
+          <li className="">
+            <label htmlFor="location" className="">
+              <input
+                type="text"
+                name="location"
+                id="location"
+                placeholder=" "
+              />
+              <span>Location</span>
+            </label>
+          </li>
+
+          <li className="">
+            <label htmlFor="summary" className="">
+              <textarea
+                name="summary"
+                id="summary"
+                placeholder="Summary"
+              ></textarea>
+            </label>
+          </li>
+
+          <li className="">
+            <label htmlFor="start-year" className="">
+              <input
+                type="text"
+                name="startYear"
+                id="start-year"
+                placeholder=" "
+              />
+              <span>From (Year)</span>
+            </label>
+          </li>
+
+          <li className="">
+            <label htmlFor="end-year" className="">
+              <input type="text" name="endYear" id="end-year" placeholder=" " />
+              <span>To (Year)</span>
+            </label>
+          </li>
+        </ul>
+      </form>
+    );
+  }
+}
+
+export default QualificationTab;
