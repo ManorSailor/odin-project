e8ed6e8910ca6c93ada92368bdc7caebd4178b87
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2023-04-12 18:27:58 +0530

    odin-cv: Render static version of the app

--- odin-javascript/odin-cv/src/App.jsx
+++ odin-javascript/odin-cv/src/App.jsx
@@ -3,12 +3,19 @@ import React from 'react';
 import './styles/styles.css';
 
 import AppHeader from './components/AppHeader';
+import Tabs from './components/Tabs';
+import CVPreview from './components/CVPreview';
 
 class App extends React.Component {
   render() {
     return (
-      <main className="">
+      <main className="main-container">
         <AppHeader />
+
+        <div className="cv-container">
+          <Tabs />
+          <CVPreview />
+        </div>
       </main>
     );
   }
