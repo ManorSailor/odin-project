5bdb9648072f0eda9f92da1df68d6adeab0cbd90
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2023-04-14 18:37:31 +0530

    odin-cv: Use list in Previews

--- odin-javascript/odin-cv/src/components/Previews/ExperiencePreview.jsx
+++ odin-javascript/odin-cv/src/components/Previews/ExperiencePreview.jsx
@@ -8,22 +8,24 @@ class ExperiencePreview extends React.Component {
       <section className="experience-preview">
         <h3 className="preview-header">Work Experience</h3>
 
-        {list.map(({ position, company, startYear, endYear, summary }) => (
-          <div className="experience-container">
-            <div className="job-details-preview">
-              <h4 className="position-preview">{position}</h4>
+        <ul className="experience-container">
+          {list.map(({ id, position, company, startYear, endYear, summary }) => (
+            <li key={id} className="">
+              <div className="job-details-preview">
+                <h4 className="position-preview">{position}</h4>
 
-              <div className="duration-preview-container">
-                <p className="company-name-preview">{company}</p>|
-                <p className="duration-preview">
-                  {startYear} - {endYear}
-                </p>
+                <div className="duration-preview-container">
+                  <p className="company-name-preview">{company}</p>|
+                  <p className="duration-preview">
+                    {startYear} - {endYear}
+                  </p>
+                </div>
               </div>
-            </div>
 
-            <p className="summary-preview">{summary}</p>
-          </div>
-        ))}
+              <p className="summary-preview">{summary}</p>
+            </li>
+          ))}
+        </ul>
       </section>
     );
   }
--- odin-javascript/odin-cv/src/components/Previews/QualificationPreview.jsx
+++ odin-javascript/odin-cv/src/components/Previews/QualificationPreview.jsx
@@ -8,19 +8,23 @@ class QualificationPreview extends React.Component {
       <section className="qualifications-preview">
         <h3 className="preview-header">Qualifications</h3>
 
-        {list.map(({ degree, university, location, startYear, endYear }) => (
-          <div className="job-details-preview">
-            <h4 className="course-preview">{degree}</h4>
+        <ul className="qualifications-preview">
+          {list.map(
+            ({ id, degree, university, location, startYear, endYear }) => (
+              <li key={id} className="job-details-preview">
+                <h4 className="course-preview">{degree}</h4>
 
-            <div className="duration-preview-container">
-              <p className="university-name-preview">
-                {university} {location}
-              </p>
-              |<p className="duration-preview">{startYear}</p>
-              <p className="duration-preview">{endYear}</p>
-            </div>
-          </div>
-        ))}
+                <div className="duration-preview-container">
+                  <p className="university-name-preview">
+                    {university} {location}
+                  </p>
+                  |<p className="duration-preview">{startYear}</p>
+                  <p className="duration-preview">{endYear}</p>
+                </div>
+              </li>
+            )
+          )}
+        </ul>
       </section>
     );
   }
